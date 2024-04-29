0c03ff055fdb228f3f3ee0574da3689d699324ee
SpaceStationALmeris11223 <125327160+SpaceStationALmeris11223@users.noreply.github.com>
2024-04-28 20:59:04 -0500

    modified:   Week 11/Bmi-Calc/src/Store/BMI-Stores.js

--- Week 11/Bmi-Calc/src/Store/BMI-Stores.js
+++ Week 11/Bmi-Calc/src/Store/BMI-Stores.js
@@ -24,5 +24,6 @@ const weightMeasurement = 'Kilograms'
 //this was the last thing i needed to figure out but oh well 
 // the program won't work but I couldnt find an awnser, I asked google, friends, I even went full hail mary and asked chat gpt if it knew how
 // unfortunatly I couldn't find a solution
+//upside i feel like I know how to use Pinia to create a larger program, with the exception of exporting variables
 
 })
